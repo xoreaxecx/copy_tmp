1f629e425c62d9740204d9c34335fb1a744c907f
Konstantin <pushad.xoreaxecx@gmail.com>
2022-02-08 23:27:55 +0300

    added log, sigint handler and some refactoring

--- copy_tmp.py
+++ copy_tmp.py
@@ -1,19 +1,58 @@
 import argparse
 import os
+import signal
 import sys
 import time
 from distutils.dir_util import copy_tree
 from shutil import copy2
 
-
 FILES_IGNORE = list()
 DIRS_IGNORE = dict()
 
 
+class Log:
+    __file = None
+
+    @staticmethod
+    def init(to_path):
+        log_name = f'__copy_tmp_log_{int(time.time())}.txt'
+        Log.__file = open(os.path.join(to_path, log_name), 'a', buffering=1)
+        # log arguments
+        Log.write(f'{" ".join(sys.argv)}\n-------')
+
+    @staticmethod
+    def write(message):
+        if Log.__file:
+            Log.__file.write(f'{message}\n\n')
+
+    # Close log handle
+    @staticmethod
+    def close():
+        if Log.__file:
+            Log.__file.close()
+
+
+# exit the program with Ctrl + C
+def signal_handler(sig, frame):
+    Log.close()
+    print('-----------------------------------\n'
+          'KeyboardInterrupt.\n'
+          'Exiting the program.\n'
+          '-----------------------------------\n')
+    sys.exit(1)
+
+
 def print_error(error):
-    print(f'=============================================================================\n'
-          f'{error}\n'
-          f'=============================================================================\n')
+    msg = (f'=============================================================================\n'
+           f'{error}\n'
+           f'=============================================================================\n')
+    print(msg)
+    Log.write(msg)
+
+
+def print_message(msg):
+    print(msg)
+    Log.write(msg)
 
 
 # capture the contents of the target directory to ignore old files.
@@ -49,7 +88,7 @@ def get_items_count(items, path_from, checkdirs):
 
 
 # attempt to copy a file.
-def copy_file(item_path, item_name, store_path, proc, rc):
+def copy_file(item_path, item_name, store_path, proc, rc, kill_once):
     global FILES_IGNORE
     done = False
     try:
@@ -58,20 +97,22 @@ def copy_file(item_path, item_name, store_path, proc, rc):
     except Exception as e:
         print_error(e)
         if proc:  # attempt to kill a blocking process.
-            kill_process(proc)
+            kill_process(proc, kill_once)
             try:
                 copy2(item_path, store_path)
                 done = True
             except Exception as e:
                 print_error(e)
         if rc and not done:  # attempt to copy a file with RawCopy.exe.
-            os.system(f'{rc} /FileNamePath:"{item_path}" /OutputPath:"{store_path}"')
+            command = f'{rc} /FileNamePath:"{item_path}" /OutputPath:"{store_path}"'
+            os.system(command)
+            print_message(f'Attempt to copy with RawCopy: {command}')
     if done:
-        print(f'file {item_name} copied.')
+        print_message(f'file {item_name} copied.')
 
 
 # attempt to copy a directory.
-def copy_dir(item_path, item_name, store_path, nested_count, proc):
+def copy_dir(item_path, item_name, store_path, nested_count, proc, kill_once):
     global DIRS_IGNORE
     done = False
     save_path = os.path.join(store_path, item_name)
@@ -81,72 +122,75 @@ def copy_dir(item_path, item_name, store_path, nested_count, proc):
     except Exception as e:
         print_error(e)
         if proc:  # attempt to kill a blocking process.
-            kill_process(proc)
+            kill_process(proc, kill_once)
             try:
                 copy_tree(item_path, save_path)
                 done = True
             except Exception as e:
                 print_error(e)
     if done:
-        print(f'dir {item_name} with {nested_count} nested items copied.')
+        print_message(f'dir {item_name} with {nested_count} nested items copied.')
 
 
 # attempt to kill the specified process.
-def kill_process(process_list):
+def kill_process(process_list, kill_once):
     killed = False
     processes = list(process_list)
     for process in processes:
         response = os.system(f'taskkill /f /im {process}')
         if response == 0:
-            print(f'{process} killed.')
+            print_message(f'{process} killed.')
             killed = True
-            if args.once:
+            if kill_once:
                 process_list.remove(process)
         elif response == 1:
-            print(f'{process} acces denided.')
+            print_message(f'{process} acces denided.')
         else:
-            print(f'{process} not found.')
+            print_message(f'{process} not found.')
     if killed:
         time.sleep(2)
 
 
 # check the input arguments.
-def check_args(arguments):
+def check_args(args):
     warnings = []
     # check "-from" dir exists and is directory.
-    if not os.path.exists(arguments.path_from):
-        warnings.append(f'Cannot access "-from" dir: {arguments.path_from}')
-    elif os.path.isfile(arguments.path_from):
-        warnings.append(f'Switch "-from" must be a dir, but a file is specified: {arguments.path_from}')
+    if not os.path.exists(args.path_from):
+        warnings.append(f'Cannot access "-from" dir: {args.path_from}')
+    elif os.path.isfile(args.path_from):
+        warnings.append(f'Switch "-from" must be a dir, but a file is specified: {args.path_from}')
     # check "-to" dir exists and is directory.
-    if os.path.isfile(arguments.path_to):
-        warnings.append(f'Switch "-to" must be a dir, but a file is specified: {arguments.path_to}')
-    elif not os.path.exists(arguments.path_to):
+    if os.path.isfile(args.path_to):
+        warnings.append(f'Switch "-to" must be a dir, but a file is specified: {args.path_to}')
+    elif not os.path.exists(args.path_to):
         try:
-            os.makedirs(arguments.path_to)
+            os.makedirs(args.path_to)
         except Exception as err:
-            warnings.append(f'Cannot create "-to" dir: {arguments.path_to}')
+            warnings.append(f'Cannot create "-to" dir: {args.path_to}')
             print_error(err)
+    # check log option
+    if not args.no_log:
+        Log.init(args.path_to)
     # check and set delay.
-    if arguments.d < 0:
-        warnings.append(f'Invalid value for "-d" switch: {arguments.d}')
-    arguments.d = arguments.d / 1000
+    if args.d < 0:
+        warnings.append(f'Invalid value for "-d" switch: {args.d}')
+    args.d = args.d / 1000
     # set exclusions.
-    arguments.exc = tuple(arguments.exc) if arguments.exc else None
+    args.exc = tuple(args.exc) if args.exc else None
     # check RawCopy arguments.
-    if arguments.rc:
+    if args.rc:
         # if rc_path is not specified, set script path + RawCopy.exe.
-        if not arguments.rc_path:
-            arguments.rc_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'RawCopy.exe')
+        if not args.rc_path:
+            args.rc_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'RawCopy.exe')
         # check the path specified is a file.
-        elif os.path.isdir(arguments.rc_path):
-            warnings.append(f'Switch "-rcpath" must be a file, but a dir is specified: {arguments.rc_path}')
+        elif os.path.isdir(args.rc_path):
+            warnings.append(f'Switch "-rcpath" must be a file, but a dir is specified: {args.rc_path}')
         # check RawCopy exists in the specified or script directory.
-        if not os.path.exists(arguments.rc_path):
-            warnings.append(f'Cannot find RawCopy.exe in the specified path: {arguments.rc_path}')
+        if not os.path.exists(args.rc_path):
+            warnings.append(f'Cannot find RawCopy.exe in the specified path: {args.rc_path}')
     else:
-        if arguments.rc_path:
-            arguments.rc_path = ''
+        if args.rc_path:
+            args.rc_path = ''
     # show warnings.
     if warnings:
         print('The following invalid switches were used:')
@@ -154,32 +198,16 @@ def check_args(arguments):
             print(f'\t{w}')
         print("Exiting the program.")
         sys.exit(2)
+    # register SIGINT handler
+    else:
+        signal.signal(signal.SIGINT, signal_handler)
+        print('-----------------------------------\n'
+              'Press Ctrl + C to exit the program.\n'
+              '-----------------------------------\n')
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='his name is Robert Paulson.')
-    parser.add_argument('-from', dest='path_from', metavar='from\\dir\\path', required=True, type=str, 
-                        help='path to the dir to catch file.')
-    parser.add_argument('-to', dest='path_to', metavar='to\\dir\\path', required=True, type=str, 
-                        help='path to the dir to store file.')
-    parser.add_argument('-checkdirs', action='store_true', 
-                        help='check dirs and their contents at 1st level.')
-    parser.add_argument('-kill', metavar='procname.exe', action='append', type=str, 
-                        help='if access denied try to kill a process by name. multiple -kill supported.')
-    parser.add_argument('-once', action='store_true', 
-                        help='kills the specified process once without repeating.')
-    parser.add_argument('-exc', metavar='extension', action='append', type=str, default=None, 
-                        help='exclude extension from monitoring. multiple -exc supported.')
-    parser.add_argument('-d', metavar='milliseconds', type=int, default=200, 
-                        help='delay between file checks in milliseconds. 200 is default.')
-    parser.add_argument('-rc', action='store_true', help='use RawCopy.exe to copy blocked files.')
-    parser.add_argument('-rcpath', dest='rc_path', metavar='path\\to\\RC', type=str, 
-                        help='path to the RawCopy.exe. "script dir\\RawCopy.exe" is default.')
-    args = parser.parse_args()
-
-    check_args(args)
-    look_around(args.path_from, args.checkdirs)
-
+def catch_files(args):
+    global FILES_IGNORE, DIRS_IGNORE
     items_count = get_items_count(os.listdir(args.path_from), args.path_from, args.checkdirs)
     while 1:
         tmp_items = os.listdir(args.path_from)
@@ -194,16 +222,43 @@ if __name__ == '__main__':
                         continue
                 path = os.path.join(args.path_from, item)
                 if os.path.isfile(path):
-                    copy_file(path, item, args.path_to, args.kill, args.rc_path)
+                    copy_file(path, item, args.path_to, args.kill, args.rc_path, args.once)
                     FILES_IGNORE.append(item)
                 else:
                     if args.checkdirs:
                         nested = os.listdir(path)
                         if item not in DIRS_IGNORE or DIRS_IGNORE[item] != nested:
-                            copy_dir(path, item, args.path_to, len(nested), args.kill)
+                            copy_dir(path, item, args.path_to, len(nested), args.kill, args.once)
                             DIRS_IGNORE[item] = nested
                     else:
-                        print(f'new dir found: {path}')
+                        print_message(f'new dir found: {path}')
                         FILES_IGNORE.append(item)
             items_count = ic
         time.sleep(args.d)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='his name is Robert Paulson.')
+    parser.add_argument('-from', dest='path_from', metavar='from\\dir\\path', required=True, type=str, 
+                        help='path to the dir to catch file.')
+    parser.add_argument('-to', dest='path_to', metavar='to\\dir\\path', required=True, type=str, 
+                        help='path to the dir to store file.')
+    parser.add_argument('-checkdirs', action='store_true', 
+                        help='check dirs and their contents at 1st level.')
+    parser.add_argument('-kill', metavar='procname.exe', action='append', type=str, 
+                        help='if access denied try to kill a process by name. multiple -kill supported.')
+    parser.add_argument('-once', action='store_true', 
+                        help='kills the specified process once without repeating.')
+    parser.add_argument('-exc', metavar='extension', action='append', type=str, default=None,
+                        help='exclude extension from monitoring. multiple -exc supported.')
+    parser.add_argument('-d', metavar='milliseconds', type=int, default=200, 
+                        help='delay between file checks in milliseconds. 200 is default.')
+    parser.add_argument('-rc', action='store_true', help='use RawCopy.exe to copy blocked files.')
+    parser.add_argument('-rcpath', dest='rc_path', metavar='path\\to\\RC', type=str, 
+                        help='path to the RawCopy.exe. "script dir\\RawCopy.exe" is default.')
+    parser.add_argument('-no-log', dest='no_log', action='store_true', help='do not create log.')
+    arguments = parser.parse_args()
+
+    check_args(arguments)
+    look_around(arguments.path_from, arguments.checkdirs)
+    catch_files(arguments)
